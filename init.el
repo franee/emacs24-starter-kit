05cb455417cae9a71cc52f662c032e5222a5f008
franee <franee@caresharing.ph>
2015-10-06 18:09:28 +0800

    franee customizations

--- init.el
+++ init.el
@@ -27,3 +27,41 @@
     (org-babel-load-file (expand-file-name "starter-kit.org" starter-kit-dir))))
 
 ;;; init.el ends here
+
+; franee customizations
+
+;; set default dir
+(setq default-directory "/opt/")
+
+(menu-bar-mode -1)
+
+;; show line numbers
+(global-linum-mode 1)
+
+;; remove trailing spaces on save
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+;; Kills live buffers, leaves some emacs work buffers
+;; optained from http://www.chrislott.org/geek/emacs/dotemacs.html
+(defun nuke-all-buffers (&optional list)
+    "For each buffer in LIST, kill it silently if unmodified. Otherwise ask.
+LIST defaults to all existing live buffers."
+    (interactive)
+    (if (null list)
+        (setq list (buffer-list)))
+    (while list
+      (let* ((buffer (car list))
+             (name (buffer-name buffer)))
+        (and (not (string-equal name ""))
+             (not (string-equal name "*Messages*"))
+             (not (string-equal name "*Buffer List*"))
+             (not (string-equal name "*buffer-selection*"))
+             (not (string-equal name "*Shell Command Output*"))
+             (not (string-equal name "*scratch*"))
+             (/= (aref name 0) ? )
+             (if (buffer-modified-p buffer)
+                 (if (yes-or-no-p
+                      (format "Buffer %s has been edited. Kill? " name))
+                     (kill-buffer buffer))
+               (kill-buffer buffer))))
+          (setq list (cdr list))))
